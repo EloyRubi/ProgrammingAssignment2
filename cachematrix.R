586feca546829a0f87f05f4c0efdf93f7b49ac47
ERubi <ee.rubi@alumnos.upm.es>
2019-02-25 15:58:21 +0100

    third chance

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are similar to the ones given in the example.
+## The idea is basically the same: save time and calculation power when the inverse
+## is already calculated. In order to accomplish this task, I have used lexical scoping,
+## which lets you work with different environments and a more flexible use of the memory
 
-## Write a short comment describing this function
+## This function creates a list of 4 objects, each of one is another function. 
+## Specifically, these functions are the "getters" and the "setters" of a typical object-
+## oriented language. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() x
+        setinverse <- function(inver) inv <<- inver
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## The possibility of working with different environments allows the function "cacheSolve"
+## to access the data in the other function, as this data belongs to de global
+## environment
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
